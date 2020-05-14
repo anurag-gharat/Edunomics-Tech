f474211ed1cda792df731b7d9bc569956f2d89cc
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-14 17:36:44 +0530

    navbar issue solved

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,8 +14,6 @@ export default function Navbar() {
     let elems = document.querySelectorAll('.dropdown-trigger');
     M.Dropdown.init(elems, 
         {inDuration: 300, outDuration: 225,hover:true,coverTrigger:false});
-
-    
     
     },[])
 
@@ -28,18 +26,26 @@ export default function Navbar() {
             <a href="/" className="brand-logo"><img className="logo" /></a>
             <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons green-text darken-4">menu</i></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to="/solution">Solution</Link></li>
+            <li><Link to="/solution">Solution</Link></li>
                 <li><Link to="/services">Services</Link></li>
-                <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Company<i className="material-icons right">arrow_drop_down</i></a></li>
-                <li><a className="dropdown-trigger" href="#!" data-target="dropdown2">Resources<i className="material-icons right">arrow_drop_down</i></a></li>
+                <li><a className="dropdown-trigger" href="#" data-target="dropdown11">Company<i className="material-icons right">arrow_drop_down</i></a></li>
+                <li><a className="dropdown-trigger" href="#" data-target="dropdown21">Resources<i className="material-icons right">arrow_drop_down</i></a></li>
                 <li><Link to="/contact">Contact</Link></li>
              </ul>
             <ul id="dropdown1" className="dropdown-content">
                 <li><Link to="/aboutus">About Us</Link></li>
                 <li><Link to="/team">Team</Link></li>
-
+            </ul>
+            <ul id="dropdown11" className="dropdown-content">
+                <li><Link to="/aboutus">About Us</Link></li>
+                <li><Link to="/team">Team</Link></li>
             </ul>
             <ul id="dropdown2" className="dropdown-content">
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="/whitepaper">White Paper</Link></li>
+                <li><Link to="/casestudies">Case Studies</Link></li>
+            </ul>
+            <ul id="dropdown21" className="dropdown-content">
                 <li><Link to="/blog">Blog</Link></li>
                 <li><Link to="/whitepaper">White Paper</Link></li>
                 <li><Link to="/casestudies">Case Studies</Link></li>
@@ -49,7 +55,7 @@ export default function Navbar() {
         </nav>
 
         <ul className="sidenav" id="mobile-demo">
-        <li><Link to="/solution">Solution</Link></li>
+         <li><Link to="/solution">Solution</Link></li>
                 <li><Link to="/services">Services</Link></li>
                 <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Company<i className="material-icons right">arrow_drop_down</i></a></li>
                 <li><a className="dropdown-trigger" href="#!" data-target="dropdown2">Resources<i className="material-icons right">arrow_drop_down</i></a></li>
