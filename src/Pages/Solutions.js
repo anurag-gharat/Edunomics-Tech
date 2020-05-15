45d139603757d85436b5d4e2b28f8dd7af70c021
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-16 00:16:10 +0530

    solutions page created

--- src/Pages/Solutions.js
+++ src/Pages/Solutions.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Heading from '../Components/Heading'
-import Panel from '../Components/Panel'
 import {AiOutlineApartment} from 'react-icons/ai'
-import {GrVmMaintenance,GrServices} from 'react-icons/gr'
-import {FaFileInvoice,FaCheckCircle,FaDesktop} from 'react-icons/fa'
+import {FaFileInvoice,FaCheckCircle,FaDesktop,FaCity} from 'react-icons/fa'
 import {MdSmartphone} from 'react-icons/md'
 import {RiShipLine} from 'react-icons/ri'
 import Card from '../Components/Card'
@@ -11,9 +9,10 @@ import Card from '../Components/Card'
 export default function Solutions() {
     return (
         <>
+        <section>
           <Heading title="Building Innovative Software Solutions for Business" />
           <div className="row">
-              <div className="container">
+              <div className="container mt-5">
                   
             <blockquote><h4>Software Solutions that we Built</h4></blockquote>
             <p>We create high-performance, interactive, user-friendly applications helping companies bring their business to a whole new level.
@@ -53,7 +52,45 @@ Software focuses on the delivery of the apps designed to enhance the clients' bu
                     </div>
                 </div>
               </div>
-          </div>  
+          </div>
+          </section>
+          <section className="center grey lighten-5">
+            <h2 className="">LET'S DISCUSS YOUR PROJECT</h2>
+            <div className="row">
+                <div className="container">
+                <form >
+                    <div className="row">
+                    <div className="input-field col s12 l6">
+                        <input className="validate" id="name" type="text"></input>
+                        <label htmlFor="name">Your Name</label>
+                    </div>
+                    <div className="input-field col s12 l6">
+                        <input className="validate" id="email" type="email"></input>
+                        <label htmlFor="email">Your Email</label>
+                    </div>
+                    </div>
+                    <div className="row">
+                        <button className="btn deep-purple white-text">Submit Response</button>
+                    </div>
+                </form>
+            
+                </div>
+            </div>
+            
+           
+          </section>
+          <div className="mt-5">
+                <div className="row">
+                <div className="container left-align">
+            <blockquote><h4>Latest Tech we apply!</h4></blockquote>
+            </div>
+                    <div className="col l4 s6 m6 offset-l4">
+                        <Card title="Enterprise Application" mystyle="white lighten-4 z-depth-1">
+                            <FaCity  className="display-2 deep-purple-text" />
+                        </Card>
+                    </div>
+                </div>
+            </div>  
         </>
     )
 }
