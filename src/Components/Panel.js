31cdcb811132e8ae9e692a3b0f895c0cad37dd90
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-15 23:04:50 +0530

    team page

--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Panel(props) {
     return (
-        <div className="">
+        <div className={props.alignment}>
             {props.children}
             <h5 className="deep-purple-text">{props.title}</h5>
             <p className="grey-text">{props.text}</p>
--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,9 +1,91 @@
 import React from 'react'
+import Heading from '../Components/Heading'
+import Panel from '../Components/Panel'
+import Img from '../Images/member1.jpg'
+
 
 export default function Team() {
     return (
-        <div>
-            
-        </div>
+        <>
+          <Heading title="Meet Our Team"/>
+            <div className="row">
+                <div className="container center">
+                    <h4 className="deep-purple-text">An Organisation is made by its team.</h4>
+                    <p>Meet our rockstars. Working across various parts of group-who have truly made it one of the most exciting place to work and create a differnce!</p>
+                </div>
+                <div className="container">
+                <div className="row ">
+                    <div className="col l3 s6 m4 ">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img}
+                            width="80%" />
+                        </Panel>
+                    </div>
+                    <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img}
+                            width="80%"  />
+                        </Panel>
+                    </div>
+                    <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%" />
+                        </Panel>
+                    </div>                   
+                     <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%" />
+                        </Panel>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%"  />
+                        </Panel>
+                    </div>
+                    <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%"  />
+                        </Panel>
+                    </div>
+                    <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%" />
+                        </Panel>
+                    </div>                   
+                     <div className="col l3 s6 m4">
+                        <Panel title="Member 1" alignment="center">
+                            <img 
+                            alt="member"
+                            className="responsive-img circle hoverable"
+                            src={Img} width="80%" />
+                        </Panel>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </>
     )
 }
