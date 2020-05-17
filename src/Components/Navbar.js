5437f4187683f8ab25b4a0c237728a6f98daa03e
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-17 23:05:25 +0530

    fianl bugs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,23 +31,23 @@ export default function Navbar() {
                 <li><a className="dropdown-trigger" href="#" data-target="dropdown21">Resources<i className="material-icons right">arrow_drop_down</i></a></li>
                 <li><Link to="/contact">Contact</Link></li>
              </ul>
-            <ul id="dropdown1" className="dropdown-content">
-                <li><Link to="/aboutus">About Us</Link></li>
-                <li><Link to="/ourteam">Team</Link></li>
+            <ul id="dropdown1" className="dropdown-content deep-purple-text">
+                <li><Link to="/aboutus" className="deep-purple-text">About Us</Link></li>
+                <li><Link to="/ourteam" className="deep-purple-text">Team</Link></li>
             </ul>
-            <ul id="dropdown11" className="dropdown-content">
-                <li><Link to="/aboutus">About Us</Link></li>
-                <li><Link to="/ourteam">Team</Link></li>
+            <ul id="dropdown11" className="dropdown-content deep-purple-text">
+                <li><Link to="/aboutus" className="deep-purple-text">About Us</Link></li>
+                <li><Link to="/ourteam" className="deep-purple-text">Team</Link></li>
             </ul>
-            <ul id="dropdown2" className="dropdown-content">
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/whitepaper">White Paper</Link></li>
-                <li><Link to="/casestudies">Case Studies</Link></li>
+            <ul id="dropdown2" className="dropdown-content deep-purple-text">
+                <li><Link to="/blog" className="deep-purple-text">Blog</Link></li>
+                <li><Link to="/whitepaper" className="deep-purple-text">White Paper</Link></li>
+                <li><Link to="/casestudies" className="deep-purple-text">Case Studies</Link></li>
             </ul>
-            <ul id="dropdown21" className="dropdown-content">
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/whitepaper">White Paper</Link></li>
-                <li><Link to="/casestudies">Case Studies</Link></li>
+            <ul id="dropdown21" className="dropdown-content deep-purple-text">
+                <li><Link to="/blog" className="deep-purple-text">Blog</Link></li>
+                <li><Link to="/whitepaper" className="deep-purple-text">White Paper</Link></li>
+                <li><Link to="/casestudies" className="deep-purple-text">Case Studies</Link></li>
             </ul>
             </div>
             
--- src/Contents/HomePage/Main.js
+++ src/Contents/HomePage/Main.js
@@ -11,7 +11,7 @@ export default function Main() {
             <h4>Edunomics Tech Solutions is a custom software development company</h4>
             <p className="grey-text darken-4">Edunomics provides turn-key software development services that add value to your business.</p>
             <p>Contact us today and transform your ideas into solutions!</p>
-            <Link className="button-secondary deep-purple darken-4  ">Contact Us</Link>
+            <Link className="button-secondary deep-purple darken-4  " to="/contact">Contact Us</Link>
             </div>
             <div className="col l6 s12">
                 <img data-aos="fade-down" src={Landing} alt="Landing Page" height="100%" width="100%"/>
