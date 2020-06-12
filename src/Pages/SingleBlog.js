1174dfc4c03324b96a1e1d464a16a9533adbedc1
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-12 22:41:57 +0530

    single blog screen updated

--- src/Pages/SingleBlog.js
+++ src/Pages/SingleBlog.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import BlockQuote from '../Components/BlockQuote'
 
-export default function SingleBlog() {
+export default function SingleBlog(props) {
+    
+    
+    const {title,subtitle,blog_content,image_url,uploadDate}=props.location.blog
+    const d = new Date(uploadDate)
     return (
         <div className="container-fluid">
             <section className="section">
-            <BlockQuote title="How to develop a webApp for Business Operations" />
+            <BlockQuote title={title} />
             <div className="container">
             <img src="https://s3-ap-south-1.amazonaws.com/redlof/think201.com/uploads/2019/02/website-vs-web-app-1024x384.png" className="responsive-img" />
             </div>
-            <p>Posted on 12 March 2019</p>
-            <h4>Some random subtitle</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Voluptatum totam impedit explicabo aliquid deleniti assumenda,
-culpa dolore saepe iusto perspiciatis velit voluptates nisi
-unde enim tempora autem numquam quisquam dolor!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Voluptatum totam impedit explicabo aliquid deleniti assumenda,
-culpa dolore saepe iusto perspiciatis velit voluptates nisi
-unde enim tempora autem numquam quisquam dolor!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Voluptatum totam impedit explicabo aliquid deleniti assumenda,
-culpa dolore saepe iusto perspiciatis velit voluptates nisi
-unde enim tempora autem numquam quisquam dolor!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Voluptatum totam impedit explicabo aliquid deleniti assumenda,
-culpa dolore saepe iusto perspiciatis velit voluptates nisi
-unde enim tempora autem numquam quisquam dolor!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Voluptatum totam impedit explicabo aliquid deleniti assumenda,
-culpa dolore saepe iusto perspiciatis velit voluptates nisi
-unde enim tempora autem numquam quisquam dolor!</p>   
+            <p>Posted on{d.toDateString()}</p>
+            <h4>{subtitle}</h4>
+            <p>
+                {blog_content}
+            </p>   
             </section>
                    
         </div>
