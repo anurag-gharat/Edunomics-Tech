d0fe8056519a8d42ae51af19ac47da7059df9aa0
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-23 01:04:51 +0530

    form added

---
+++ src/Pages/AffliateForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Heading from '../Components/Heading'
+
+export default function AffliateForm() {
+    return (
+        <div className="p-t-100">
+            <div className="container">
+                <Heading title="Edunomics Business Affiliate" />
+            </div>
+            <div className="container z-depth-2">
+                
+            </div>
+            
+        </div>
+    )
+}
